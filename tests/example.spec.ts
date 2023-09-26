c33a8d43aadcc25f0c79902764492c8194c1432d
Jason Murray <umurja@gmail.com>
2023-09-26 11:36:52 -0400

    Added Console reporting, but currently commented out w/ issues.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+/*
 const headerItems = ['About', 'Store', 'Gmail'];
 for (const item of headerItems) {
 test(`Header link 1 - ${item}`, async ({ page }) => {
@@ -11,6 +12,7 @@ test(`Header link 1 - ${item}`, async ({ page }) => {
   // Assert url is as expected
   await expect(page.url().split('?')[0]).toEqual(expectedUrl(`${item}`));
 })};
+*/
 
 const testData  = [
   { headerItem: 'About', expectedUrl: 'https://about.google/' },
@@ -20,12 +22,37 @@ const testData  = [
   for (const { headerItem, expectedUrl} of testData) 
   test(`Header link 2 - ${ headerItem}`, async ({ page }) => {
     await page.goto('https://www.google.com/');
-  
+ /* TODO: Console stuff to figure out later
+// Listen for all console logs
+page.on('console', msg => console.log(msg.text()));
+
+// Listen for all console events and handle errors
+page.on('console', msg => {
+  if (msg.type() === 'error')
+    console.log(`Error text: "${msg.text()}"`);
+});
+
+// Get the next console log
+const msgPromise = page.waitForEvent('console');
+await page.evaluate(() => {
+  console.log('hello', 42, { foo: 'bar' });  // Issue console.log inside the page
+});
+const msg = await msgPromise;
+
+// Deconstruct console log arguments
+await msg.args()[0].jsonValue(); // hello
+await msg.args()[1].jsonValue(); // 42
+*/
+
+
   // Click the header Link.
     await page.getByRole('link', {name: headerItem}).click();
   
   // Assert url is as expected 
   expect(page.url().split('?')[0]).toEqual(expectedUrl);
+
+  
+
   
   });
 
